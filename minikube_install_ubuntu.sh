378e43056cb917574d7f42a6671fd2c9f299d79c
Bhavesh Shah <22.bhavesh@gmail.com>
2024-04-15 18:12:34 +0530

    Update minikube_install_ubuntu.sh

--- minikube_install_ubuntu.sh
+++ minikube_install_ubuntu.sh
@@ -4,10 +4,10 @@ sudo apt install -y curl wget apt-transport-https
 sudo apt install -y docker.io
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER && newgrp docker
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo chmod +x minikube
 sudo mv minikube /usr/local/bin/
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
-minikube start --driver=docker
+sudo minikube start --driver=docker
